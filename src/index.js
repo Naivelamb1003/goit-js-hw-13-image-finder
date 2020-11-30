d50925da0804fd1dcb32f0e45fb410ca1dba36de
Malik Nadiia <59849318+Naivelamb1003@users.noreply.github.com>
2020-12-01 01:50:18 +0200

    подлючен css(добавить стиль кнопки)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
 import ImageApiService from './api-service';
 import { debounce } from 'debounce';
 import imageCardTp from './templates/cardImage.hbs';
+import modalImage from './open-modal.js';
+import './main.css';
+import './images/icon-close.svg';
+import { alert, Stack } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const formText = document.querySelector('#search-form');
 const searchLine = document.querySelector('.search-line');
 const contentList = document.querySelector('#list-cont');
 const loadMoreBtn = document.querySelector('.button-more');
+
+const controlModalImage = new modalImage();
+controlModalImage.init();
+
 let ApiService = null;
+loadMoreBtn.disabled = true;
 
 const debounceRenderImage = debounce(function () {
+  
+  loadMoreBtn.disabled = false;
   ApiService = new ImageApiService();
-  ApiService.searchQuery = searchLine.value;
-  ApiService.fetchArticles().then(renderImageCard).catch(onFetchError);
+  if(searchLine.value !='') {
+    ApiService.searchQuery = searchLine.value;
+    ApiService.fetchArticles().then(renderImageCard).catch(onFetchError);
+  }
+    
 }, 500);
 
-formText.addEventListener('input', debounceRenderImage);
-
+searchLine.addEventListener('input', debounceRenderImage);
 formText.addEventListener('submit', submitSechPicter);
 loadMoreBtn.addEventListener('click', fetchImages);
+
+const myStack = new Stack({
+  dir1: 'down',
+  dir2: 'left',
+  firstpos1: 25,
+  firstpos2: 25,
+  spacing1: 36,
+  spacing2: 36,
+});
 
 function submitSechPicter(e) {
   e.preventDefault();
@@ -26,13 +50,29 @@ function submitSechPicter(e) {
 
 function renderImageCard(response) {
   let markup = null;
-  markup = imageCardTp(response);
-  contentList.innerHTML = markup;
-  console.log(response);
+  if (response.status == 404){
+    onNotFoundError();
+    }else{
+      markup = imageCardTp(response);
+      contentList.innerHTML = markup;
+    }
+   
 }
 
 function onFetchError(response) {
-  console.log(response);
+  if(searchLine.value === ''){
+    alert({
+      text: 'Lost connection',
+      stack: myStack,
+    });
+  }
+}
+
+function onNotFoundError() {
+  alert({
+    text: 'Images not found',
+    stack: myStack,
+  });
 }
 
 function fetchImages() {
